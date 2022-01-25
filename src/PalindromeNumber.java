abe687e4f92d4d454006cc27699d3165a5a16f7e
Jeong-Uk Park <dnr1105@gmail.com>
2022-01-25 23:01:09 +0900

    Add temp code

---
+++ src/PalindromeNumber.java
@@ -0,0 +1,15 @@
+public class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        boolean ans = false;
+
+
+    }
+
+    public static void main(String[] args){
+        PalindromeNumber pn = new PalindromeNumber();
+
+        int x = 121;
+        pn.isPalindrome(x);
+
+    }
+}
--- src/Solution.java
+++ src/Solution.java
@@ -19,7 +19,7 @@ class S {
     }
 }
 
-public class Solution{
+class Solution{
     public static void main(String[] args) {
         int[] nums = {2, 7, 11, 15};
         int target = 9;
